974286dc4b4fcd45ede0306ebb722e95bd4edded
Ushupiuck <giancarloknd@hotmail.com>
2026-04-27 05:33:02 -0300

    Update 16 HTZ Platform.asm

--- obj/16 HTZ Platform.asm
+++ obj/16 HTZ Platform.asm
@@ -16,13 +16,7 @@ Obj16_Init:
 		addq.b	#2,obRoutine(a0)
 		move.l	#Map_Obj16,obMap(a0)
 		move.w	#make_art_tile(ArtTile_HtzZipline,2,0),obGfx(a0)
-		bsr.w	Adjust2PArtPointer
-	if FixBugs
 		ori.b	#4,obRender(a0)
-	else
-		; Bug: This does not correctly flip the object.
-		move.b	#4,obRender(a0)
-	endif
 		move.b	#32,obActWid(a0)
 		move.b	#0,obFrame(a0)
 		move.b	#1,obPriority(a0)
@@ -37,17 +31,11 @@ Obj16_Main:
 		move.w	#-$28,d3
 		move.w	(sp)+,d4
 		bsr.w	sub_F78A
-	if FixBugs=0
-		; Redundant line of code.
-		move.w	objoff_30(a0),d0
-	endif
-		out_of_range.w	JmpTo_DeleteObject
-		jmpto	JmpTo_DisplaySprite
+		out_of_range.w	DeleteObject
+		jmp	(DisplaySprite).l
 
-	if RemoveJmpTos
 JmpTo_DeleteObject	; JmpTo
 		jmp	(DeleteObject).l
-	endif
 
 ; =============== S U B	R O U T	I N E =======================================
 
@@ -73,15 +61,11 @@ Obj16_InitMove:
 		beq.s	locret_151BE
 		addq.b	#1,obSubtype(a0)
 		move.w	#$200,obVelX(a0)
-	if FixBugs
 		btst	#0,obStatus(a0)
 		beq.s	.facingright
 		neg.w	obVelX(a0)
 
 .facingright:
-	else
-		; This object does not work when being flipped horizontally.
-	endif
 		move.w	#$100,obVelY(a0)
 		move.w	#160,objoff_34(a0)	; set wait time to 5.3 seconds
 
@@ -90,7 +74,7 @@ locret_151BE:
 ; ---------------------------------------------------------------------------
 
 Obj16_Move:
-		jsrto	JmpTo_ObjectMove
+		jsr	(ObjectMove).l
 		subq.w	#1,objoff_34(a0)
 		bne.s	locret_151CE
 		addq.b	#1,obSubtype(a0)
